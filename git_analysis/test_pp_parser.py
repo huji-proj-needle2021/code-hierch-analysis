39f454d6de7cc39971dfc2cfbea663ce1ee4a718
Daniel Kerbel <nmdanny@gmail.com>
2022-02-09 17:31:06 +0200

    wip

--- git_analysis/test_pp_parser.py
+++ git_analysis/test_pp_parser.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
+from typing import Union
 from logging import basicConfig
 import pyparsing as pp
+# pp.ParserElement.enable_packrat()
+
 from typing import List, NamedTuple, Optional
 
 class Method(NamedTuple):
@@ -12,12 +15,10 @@ class Class(NamedTuple):
     name: str
     start: int
     end: int
-    methods: List[Method]
-    classes: List[Class]
+    members: List[Union[Class, Method]]
 
 class JavaFile(NamedTuple):
     start: int
-    end: int
     classes: List[Class]
     package_name: Optional[str] = None
 
@@ -30,7 +31,7 @@ SEMICOLON = pp.Literal(";").suppress()
 LBRACE, RBRACE = map(pp.Suppress, map(pp.Literal, ["{", "}"]))
 LPAREN, RPAREN = map(pp.Suppress, map(pp.Literal, ["(", ")"]))
 
-identifier = pp.Word(pp.alphas+"_", pp.alphanums+"_")
+identifier = pp.Word(pp.alphas+"_$", pp.alphanums+"_$")
 comment = (single_line_comment | multiline_comment).suppress()
 
 # we don't really care about modifiers, but putting them might
@@ -52,138 +53,172 @@ modifier = pp.one_of([
 ], as_keyword=True)
 
 
+# types
+
+# generic, array, namespaced, etc...
+opt_type_parameters = pp.Opt(pp.nested_expr("<", ">"))
+
+inner_type = pp.delimited_list(identifier + opt_type_parameters, ".", min=1)
+type_ = (inner_type + (pp.Literal("[") + pp.Literal("]"))[...]).suppress()
+
 # method
 
 # we don't care about a method's body, but need to deal with the fact it might
 # have even more nested brackets
 method_body = pp.nested_expr("{", "}").suppress()
 
+parameter_list = LPAREN + ... + RPAREN
 opt_throws = pp.Opt(pp.Keyword("throws") + pp.SkipTo(method_body | SEMICOLON)).suppress()
-ctor = identifier("name") + LPAREN + ... + RPAREN + opt_throws + method_body
+ctor = identifier("name") + parameter_list + opt_throws + method_body
 
 # an interface method may end with a semicolon rather than a block
-method_declaration = identifier + identifier("name") + LPAREN + ... + RPAREN + opt_throws + (method_body | SEMICOLON)
-ctor_or_method_declaration = modifier[...] + (ctor | method_declaration)
+method_declaration = type_ + identifier("name") + parameter_list + opt_throws + (method_body | SEMICOLON)
+ctor_or_method_declaration = pp.Located(modifier[...] + opt_type_parameters + (ctor | method_declaration))
 
-def method_parse_action(s: str, loc: int, res: pp.ParseResults) -> Method:
+def method_parse_action(res: pp.ParseResults) -> Method:
     return Method(
-        start=loc,
-        end=loc+len(s),
-        name=res["name"]
+        start=res["locn_start"],
+        end=res["locn_end"],
+        name=res["value"]["name"]
     )
 ctor_or_method_declaration.set_parse_action(method_parse_action)
-
-
+ctor_or_method_declaration.ignore(comment)
 
 
 # class/interface/enum class/annotation etc..
 
-class_body = pp.Forward()
-class_declaration = modifier[...] + pp.one_of(["enum", "class", "interface", "@interface"], as_keyword=True) + identifier("name") + ... + class_body 
+class_declaration = pp.Forward()
+class_member = pp.Forward()
 
-class_member = class_declaration | ctor_or_method_declaration
-class_member = pp.SkipTo(class_member).suppress() + class_member
-class_body <<= LBRACE + pp.Group(class_member[...])("class_body") + RBRACE
+class_member <<= class_declaration | ctor_or_method_declaration
 
-# TODO: mysterious bug when using the following form 
-# class_declaration = modifier[...] + pp.one_of(["enum", "class", "interface", "@interface"], as_keyword=True) + identifier("name") + ... + LBRACE + class_body + RBRACE 
-# class_body <<= pp.Group(class_member[...])("class_body")
+class_body = LBRACE + pp.Group(class_member[...])("classes") + RBRACE
+# opt_extends = pp.Opt(pp.Keyword("extends") + pp.delimited_list(type_, delim=",", combine=True)).suppress()
+# opt_implements = pp.Opt(pp.Keyword("implements") + pp.delimited_list(type_, delim=",", combine=True)).suppress()
+class_declaration <<= pp.Located(modifier[...] + pp.one_of(["enum", "class", "interface", "@interface"],
+                               as_keyword=True) + identifier("name") + ... + class_body)
 
-def class_declaration_parse_action(s: str, loc: int, res: pp.ParseResults) -> Class:
+
+def class_declaration_parse_action(res: pp.ParseResults) -> Class:
+    value = res["value"]
     return Class(
-        name=res["name"],
-        start=loc,
-        end=loc+len(s),
-        methods=[m for m in res["class_body"] if isinstance(m, Method)],
-        classes=[c for c in res["class_body"] if isinstance(c, Class)]
+        name=value["name"],
+        start=res["locn_start"],
+        end=res["locn_end"],
+        members=[v for v in value["classes"] if isinstance(v, Class) or isinstance(v, Method)]
     )
 class_declaration.set_parse_action(class_declaration_parse_action)
+class_declaration.ignore(comment)
 
 # entire file
 
 package = pp.Keyword("package") + pp.delimited_list(identifier, ".", combine=True) + SEMICOLON
-compilation_unit = pp.Opt(package)("package") + ... + class_declaration[...]("classes")
+package.ignore(comment)
 
-def compilation_unit_parse_action(s: str, loc: int, res: pp.ParseResults) -> JavaFile:
-    return JavaFile(
-        package_name=res["package"][1] if res["package"] else None,
-        start=loc,
-        end=loc+len(s),
-        classes=[c for c in res["classes"] if isinstance(c, Class)]
-    )
-compilation_unit.add_parse_action(compilation_unit_parse_action)
+
+def test_pp_type():
+    types = [
+        "Foo",
+        "T[]",
+        "ArrayList<Foo>[][]",
+        "ArrayList<ArrayList<T>[][]>[][][]"
+    ]
+    for t in types:
+        type_.parse_string(t, parse_all=True)
 
 def test_pp_method_parse():
-    some_method = """retType theFunction(i don't care...) throws something { 
+    some_method = """static <T wat> Map<Wow, T[][]> theFunction(i don't care...) throws something { 
         don't care about what's inside at all
         even if there are more brackets inside {}
         or quoted brackets which we don't care about either: "{}" "{
-
         }"
         }"""
-    method_obj = method_declaration.parse_string(some_method, parse_all=True)
+    method_obj = ctor_or_method_declaration.parse_string(some_method, parse_all=True)[0]
     assert method_obj.name == "theFunction"
 
     ctor = "public Main() {}"
-    ctor_obj = method_declaration.parse_string(ctor, parse_all=True)
+    ctor_obj = ctor_or_method_declaration.parse_string(ctor, parse_all=True)[0]
     assert ctor_obj.name == "Main"
-    
-def test_pp_class_parse():
 
-    empty_class = "class Foo{}"
-    res: Class = class_declaration.parse_string(empty_class, parse_all=True)[0]
-    assert res.name == "Foo"
-
-
-    basic_class = """class MyClass implements some junk and watnot {
-        {}
-        junk 
-        ;{{}}
-        more junk
-        LikeACtor() {
+def test_pp_lone_class():
+    raw = """
+    class Class {
+        int foo() {
 
         }
-        {}
-        even junk with braces {{}}
-        static {}
-        {}
-        void bar() {
-
-        }
-    }"""
-    res: Class = class_declaration.parse_string(basic_class, parse_all=True)[0]
-    assert res.name == "MyClass"
-    assert res.methods[0].name == "LikeACtor"
-    assert res.methods[1].name == "bar"
-
-def test_pp_nested_class():
-    nested_class = """class MyClass implements some junk and stuff {
-        class InnerClass {
-            enum EvenMoreNested {
-                class StillNested {
-                    void EvenHasAFunction() {}
-                }
-            }
-        } 
-        class Kek {
-            class Mix {}
-        }
-    }"""
-    res: Class = class_declaration.parse_string(nested_class, parse_all=True)[0]
-    assert res.name == "MyClass"
-    # assert res.classes[0].name == "InnerClass"
-    # assert res.classes[1].name == "Another"
-
-def test_pp_compilation_unit():
-    file = """
-    package com.foo.bar;
-
-    class Foo {
-        void fooFunc() {}
     }
     """
 
-    res: JavaFile = compilation_unit.parse_string(file, parse_all=True)[0]
-    assert res.package_name == "com.foo.bar"
-    assert res.classes[0].name == "Foo"
-    assert res.classes[0].methods[0].name == "fooFunc"
+    res = class_declaration.parse_string(raw, parse_all=True)[0]
+    assert res.name == "Class"
+
+def test_pp_generic_lone_class():
+    raw = """
+    class Class<T> implements Foo<T> {
+        int foo() {
+
+        }
+    }
+    """
+
+    res = class_declaration.parse_string(raw, parse_all=True)[0]
+    assert res.name == "Class"
+
+def test_pp_single_nested_class():
+    raw = """
+    class Class {
+        class Inner {
+            void foo() {
+
+            }
+        }
+    }
+    """
+
+    res: Class = class_declaration.parse_string(raw, parse_all=True)[0]
+    assert res.name == "Class"
+    assert res.members[0].name == "Inner"
+    assert res.members[0].members[0].name == "foo"
+
+
+def test_pp_multiple_nested_classes():
+    raw = """
+    class Class {
+        class Inner {
+            void foo() {
+
+            }
+        }
+        class SecondInner {
+            int bar() {}
+        }
+        void aMethod() {
+
+        }
+        class AnotherClass {
+
+        }
+    }
+    """
+
+    res: Class = class_declaration.parse_string(raw, parse_all=True)[0]
+    assert res.name == "Class"
+    assert res.members[0].name == "Inner"
+    assert res.members[0].members[0].name == "foo"
+    assert res.members[1].name == "SecondInner"
+    assert res.members[1].members[0].name == "bar"
+    assert res.members[2].name == "aMethod"
+    assert res.members[3].name == "AnotherClass"
+    
+def test_pp_class_with_unrecognized_elements():
+    raw = """
+    class Class {
+        int notInMyGrammar = doo();
+        @Override // nor is this one
+        void foo() {}
+    }
+    """
+
+    res: Class = class_declaration.parse_string(raw, parse_all=True)[0]
+    assert res.name == "Class"
+    assert res.members[0].name == "foo"
