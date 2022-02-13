74de2843cec88818d50ae63d0e2cd2453cbe987c
Daniel Kerbel <nmdanny@gmail.com>
2022-02-14 01:50:40 +0200

    refactor a bit

--- git_analysis/java_change.py
+++ git_analysis/java_change.py
@@ -1,33 +1,43 @@
 """ This file defines a few types needed to represent a change in the Java source code,
-    which will act as the items in the itemset algorithm
+    which will act as the items in the itemset algorithm, and a class for matching
+    byte positions to the Java hierarchy they belong to.
 """
-from typing import NamedTuple, Optional
+from typing import NamedTuple, Optional, Tuple, List
 from enum import IntEnum
+from .java_type import JavaHierarchy, JavaIdentifier, HierarchyType, JavaFile, JavaClass, JavaMethod
 
-class JavaIdentifier(NamedTuple):
-    """ An identifier to some unit of Java code that corresponds to one or more
-        hierarchies in program modularization.
-        For this project we look at 3 hierarchies:
+class PosToJavaUnitMatcher:
+    """ Responsible for matching byte positions in the original file, to
+        the Java hierarchy that they belong to. Positions are assumed to be given in an ascending order"""
+    def __init__(self, root: JavaHierarchy):
+        self._it = root.iterate_dfs_preorder()
+        self._cur = next(self._it)
+        self._next = next(self._it, None)
 
-        - Java Package
-        - Java Class 
-        - Java Method/Function (we make no distinction between static and non-static methods)
+    def find(self, pos: int) -> JavaHierarchy:
+        """ Finds the most specific Java hierarchy to which the byte at given
+            position belongs to. 
+        """
+        assert pos >= self._cur.start, "Byte positions must be given in weakly increasing order"
 
-        More specific hierarchies belong to their outer ones - a method must have a class and and a package,
-        a class must have a package.
+        while self._next is not None and pos >= self._next.start:
+            self._cur = self._next
+            self._next = next(self._it, None)
 
-        TODO: nested classes (multiple levels of classnames)
-        TODO: break up a package into its components (e.g, com.foo.A belongs to package 'foo' belongs to package 'com')
-
-    """
-    package: str
-    class_name: Optional[str] = None
-    method_name: Optional[str] = None
-
+        # If the position is beyond the current declaration, but before the next one(
+        # or the next one doesn't exist) - e.g, a change between two adjacent methods,
+        # or after the last method, then we must belong to the parent (e.g, the class
+        # containing them)
+        candidate = self._cur
+        while pos >= candidate.end and candidate.parent is not None:
+            candidate  = candidate.parent
+        return candidate
 
 class ChangeType(IntEnum):
     """ The kind of change reflected by a git diff line. """
     ADD = 0
+    # When a java identifier is deleted and added in the same commit, we consider 
+    # it modified
     MODIFY = 1 
     DELETE = 2
     UNSUPPORTED = 3
@@ -38,3 +48,28 @@ class JavaChange(NamedTuple):
     """
     changed_hierarchy: JavaIdentifier
     change_type: ChangeType
+
+def test_pos_to_java_unit_matcher():
+    tree = JavaFile(start=0, end=200, name="F", members=[])
+    c1 = JavaClass(
+            start=1, end=100, name="C1", kind="class", members=[], parent=tree)
+    c1.members.extend([
+        JavaMethod(start=5, end=40, name="C1M1", parent=c1),
+        JavaMethod(start=41, end=80, name="C1M2", parent=c1)
+    ])
+    c2 = JavaClass(
+            start=105, end=180, name="C2", kind="class", members=[], parent=tree)
+    c2.members.extend([JavaMethod(start=110, end=140, name="C2M1", parent=c2),
+                        JavaMethod(start=150, end=180, name="C2M2", parent=c2)
+                        ])
+    tree.members.extend([c1,c2])
+    finder = PosToJavaUnitMatcher(tree)
+    assert finder.find(0).name == "F"
+    assert finder.find(1).name == "C1"
+    assert finder.find(40).name == "C1"
+    assert finder.find(41).name == "C1M2"
+    assert finder.find(85).name == "C1"
+    assert finder.find(100).name == "F"
+    assert finder.find(120).name == "C2M1"
+    assert finder.find(160).name == "C2M2"
+    assert finder.find(180).name == "F"
--- git_analysis/java_type.py
+++ git_analysis/java_type.py
@@ -1,9 +1,12 @@
 """ Defines the hierarchy of Java units of code - a package(file) may include one or more
     classes(/enums/interfaces), each may have further classes or methods.
+
+    Includes `JavaHierarchy which is essentially a tree node, and `JavaIdentifier`
+    which is a compact representation of said node
 """
 
 from __future__ import annotations
-from typing import NamedTuple, List, Optional, Union, Iterator
+from typing import NamedTuple, List, Optional, Union, Iterator, Tuple
 from enum import IntEnum, auto
 from functools import partial
 
@@ -16,6 +19,13 @@ class HierarchyType(IntEnum):
     
 
 class JavaHierarchy:
+    """ A node in our Java parse tree, represents a hierarchy of a java unit of code:
+        a package, type definition(class, interface, enum) or a method
+        
+        Includes the byte position in which the declaration of this hierarchy starts and
+        ends, as well as references to member and parent nodes in the hierarchy inferred
+        from the parsed file.
+    """
     start: int
     end: int
     name: str
@@ -33,51 +43,44 @@ class JavaHierarchy:
         self.parent = parent
         self.members = [] if members is None else members
 
+    def iterate_dfs_preorder(self) -> Iterator[JavaHierarchy]:
+        """ Traverses the Java hierarchy in a DFS preorder (a topological sort), note that
+            this ordering goes over the Java hierarchy elements by the order in which their declarations appear.
+        """
+        stack = [self]
+        while len(stack) > 0:
+            parent = stack.pop()
+            if parent.members:
+                stack.extend(reversed(parent.members))
+            yield parent
+
 
 JavaFile = partial(JavaHierarchy, kind=HierarchyType.package)
 JavaClass = partial(JavaHierarchy, kind=HierarchyType.type_def)
 JavaMethod = partial(JavaHierarchy, kind=HierarchyType.method)
 
-
-def tree_dfs_preorder(tree: JavaHierarchy) -> Iterator[JavaHierarchy]:
-    """ Traverses the Java hierarchy in a DFS preorder (a topological sort), note that
-        this ordering goes over the Java hierarchy elements by the order in which their declarations appear.
+class JavaIdentifier:
+    """ An identifier to some unit of Java code that corresponds to one or more
+        hierarchies in program modularization. For example `com.foo.ClsA.bar`
+        is an identifier to method `bar` under `ClsA` belonging to package `com.foo`,
+        this class represents such identifier in an object form.
     """
-    stack = [tree]
-    while len(stack) > 0:
-        parent = stack.pop()
-        if parent.members:
-            stack.extend(reversed(parent.members))
-        yield parent
 
+    # Java hierarchy kinds and identifiers, from most specific to most general.
+    hierarchies: List[Tuple[HierarchyType, str]]
+    __slots__ = ['hierarchies']
 
+    def __init__(self, bottommost_hierarchy: JavaHierarchy):
+        self.hierarchies = [(bottommost_hierarchy.kind, bottommost_hierarchy.name)]
+        parent = bottommost_hierarchy.parent
+        while parent is not None:
+            self.hierarchies.append((parent.kind, parent.name))
+            parent = parent.parent
 
-class PosToJavaUnitMatcher:
-    """ Responsible for matching byte positions in the original file, to
-        the Java hierarchy that they belong to. Positions are assumed to be given in an ascending order"""
-    def __init__(self, root: JavaHierarchy):
-        self._it = tree_dfs_preorder(root)
-        self._cur = next(self._it)
-        self._next = next(self._it, None)
-
-    def find(self, pos: int) -> JavaHierarchy:
-        assert pos >= self._cur.start, "Byte positions must be given in weakly increasing order"
-
-        while self._next is not None and pos >= self._next.start:
-            self._cur = self._next
-            self._next = next(self._it, None)
-
-        # If the position is beyond the current declaration, but before the next one(
-        # or the next one doesn't exist) - e.g, a change between two adjacent methods,
-        # or after the last method, then we must belong to the parent (e.g, the class
-        # containing them)
-        candidate = self._cur
-        while pos >= candidate.end and candidate.parent is not None:
-            candidate  = candidate.parent
-        return candidate
+    def __str__(self) -> str:
+        return ".".join(tup[1] for tup in reversed(self.hierarchies))
 
     
-
 def test_dfs():
     tree = JavaFile(start=0, end=200, name="F", members=[])
     c1 = JavaClass(
@@ -92,16 +95,8 @@ def test_dfs():
                         JavaMethod(start=150, end=180, name="C2M2", parent=c2)
                         ])
     tree.members.extend([c1,c2])
-    names = [h.name for h in tree_dfs_preorder(tree)]
+    names = [h.name for h in tree.iterate_dfs_preorder()]
     assert names == ["F", "C1", "C1M1", "C1M2", "C2", "C2M1", "C2M2"]
 
-    finder = PosToJavaUnitMatcher(tree)
-    assert finder.find(0).name == "F"
-    assert finder.find(1).name == "C1"
-    assert finder.find(40).name == "C1"
-    assert finder.find(41).name == "C1M2"
-    assert finder.find(85).name == "C1"
-    assert finder.find(100).name == "F"
-    assert finder.find(120).name == "C2M1"
-    assert finder.find(160).name == "C2M2"
-    assert finder.find(180).name == "F"
+    assert str(JavaIdentifier(c1)) == "F.C1"
+    assert str(JavaIdentifier(c2.members[1])) == "F.C2.C2M2"
