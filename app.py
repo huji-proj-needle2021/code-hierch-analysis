8198d3259374bdda1f8aab5a1cf90c368c7dc400
Daniel Kerbel <nmdanny@gmail.com>
2022-02-22 23:41:00 +0200

    graph display via dash kinda works

--- app.py
+++ app.py
@@ -5,65 +5,98 @@ from re import search
 import tempfile
 from typing import Optional
 from dash import Dash, html, dcc, Input, Output, State
+from dash.development.base_component import Component
 from dash.exceptions import PreventUpdate
 import dash_cytoscape as cyto
 import plotly.express as px
 import pandas as pd
-from graph_import import Args, GEN_CALLGRAPH_JAR, JAR_FOLDER, OUTPUT_FOLDER
+from git_analysis.java_type import HierarchyType
+from graph_import import Args, GEN_CALLGRAPH_JAR, JAR_FOLDER, OUTPUT_FOLDER, RawGraph, raw_graph_to_igraph, igraph_to_cytoscape_fig
 from pathlib import Path
 import logging
 
 app = Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+def field(label: str, input: Component):
+    return html.Div(children=[
+        html.Label(children=label, htmlFor=input.id),
+        input
+    ], className="formField")
 
-# import_args = Args(
-#     edge_filter=["jadx"],
-#     jar_filter=["jadx"],
-#     input_jar_folder=JAR_FOLDER,
-#     graph_output_folder=OUTPUT_FOLDER,
-#     main_class_identifier="jadx.gui.JadxGUI",
-# )
+graph_create_status = html.Div(children=[
+    dcc.Loading(id="creating", type="default", children=[
+        html.H3(id="graph_state")
+    ])
+])
+
+graph_create = html.Div(children=[
+    html.H2(children="Callgraph generation options"),
+
+    # html.Label(children="Graph directory where the call graph will be created and imported from", htmlFor="out_dir"),
+    # dcc.Input(id="out_dir", type="text", placeholder="Folder that will contain graph data",
+    #           value="graphImport-jadx"),
+    field("Output folder for generated callgraph (where it can be imported from)",
+          dcc.Input(id="out_dir", type="text", placeholder="Folder that will contain graph data",
+                    value="graphImport-jadx")),
+
+    field("Folder that contains .jar files for generating a call graph",
+          dcc.Input(id="jar_dir", type="text", placeholder="Folder containing .jar files",
+                    value="C:\\dev\\genCallgraph\\toAnalyze")),
+
+    field("Identifier of main class (that contains the 'main' function)",
+          dcc.Input(id="main_identifier", type="text", placeholder="Type a fully qualified class name, e.g, com.foo.MainClass",
+                    value="jadx.gui.JadxGUI")),
+
+    field("Words that should appear in a .jar file. A file without any of these words will be skipped",
+          dcc.Dropdown(id="jar_filter", multi=True, placeholder="Type any word, e.g 'jadx' ",
+                       value=["jadx"])),
+
+
+    field("Words that should appear in an edge's source and target identifiers. An edge without any of these words will be skipped",
+          dcc.Dropdown(id="edge_filter", multi=True, placeholder="Type any word, e.g 'jadx' ",
+                       value=["jadx"])),
+
+    html.Button("Create callgraph", id="create_cg"),
+    graph_create_status,
+])
+
+
+field("Java Hierarchy",
+      dcc.Dropdown(id="hierch", placeholder="Type any word, e.g 'jadx' ",
+                   options={
+                                            variant.name: variant.name
+                                            for variant in HierarchyType},
+                   value=HierarchyType.method.name),
+      )
+
+
+
+DEFAULT_MAX_NODES = 1000
+DEFAULT_MAX_EDGES = 1000
 
 graph_import = html.Div(children=[
-    html.H2(children="Callgraph import options"),
-
-    html.Label(children="Graph directory where the call graph will be created and imported from", htmlFor="out_dir"),
-    dcc.Input(id="out_dir", type="text", placeholder="Folder that will contain graph data",
-              value="graphImport"),
-
-
-    html.Label(children="Folder that contains .jar files for generating a call graph", htmlFor="jar_dir"),
-    dcc.Input(id="jar_dir", type="text", placeholder="Folder containing .jar files",
-              value="C:\\dev\\genCallgraph\\toAnalyze"),
-
-    html.Label(children="Identifier of main class (that contains the 'main' function)", htmlFor="main_identifier"),
-    dcc.Input(id="main_identifier", type="text", placeholder="Type a fully qualified class name, e.g, com.foo.MainClass",
-              value="jadx.gui.JadxGUI"),
-
-    html.Label(children="Words that should appear in a .jar file. A file without any of these words will be skipped", htmlFor="jar_filter"),
-    dcc.Dropdown(id="jar_filter", multi=True, placeholder="Type any word, e.g 'jadx' "),
-
-    html.Label(children="Words that should appear in an edge's source and target identifiers. An edge without any of these words will be skipped", htmlFor="edge_filter"),
-    dcc.Dropdown(id="edge_filter", multi=True, placeholder="Type any word, e.g 'jadx' ",
-                 value=["jadx"]),
-
-    html.Button("Import", id="import"),
-    dcc.Store(id="callgraph", storage_type="memory")
+    html.H2(children="Graph import and visualization options"),
+    field("Graph import directory",
+          dcc.Input(id="import_dir", type="text", placeholder="Folder that contains graph data",
+                    value="graphImport-jadx")
+          ),
+    field("Java Hierarchy",
+          dcc.Dropdown(id="hierch", placeholder="Type any word, e.g 'jadx' ",
+                       options={
+                           variant.name: variant.name
+                           for variant in HierarchyType},
+                       value=HierarchyType.method.name),
+          ),
+    field("Maximal number of nodes",
+          dcc.Slider(id="max_nodes", min=0, max=DEFAULT_MAX_NODES)),
+    field("Maximal number of edges",
+          dcc.Slider(id="max_edges", min=0, max=DEFAULT_MAX_EDGES)),
+    html.Button("Import graph", id="import")
 ])
 
-graph_state_div = html.Div(children=[
-    html.H2(id="graph_state")
-])
-
+# a hacky way of using a dropdown input
+# for arbitrary input
 def multi_input(search_value, value):
     if not search_value:
         raise PreventUpdate
@@ -83,15 +116,19 @@ edge_filter = app.callback(
 )(multi_input)
 
 @app.callback(
-    Output("callgraph", "data"),
-    Input("import", "n_clicks"),
+    dict(graph_state=Output("graph_state", "children"),
+         import_dir=Output("import_dir", "value")
+         ),
+    Input("create_cg", "n_clicks"),
     State("out_dir", "value"),
     State("jar_dir", "value"),
     State("edge_filter", "value"),
     State("jar_filter", "value"),
     State("main_identifier", "value"),
+    State("graph_state", "children"),
+    State("import_dir", "value")
 )
-def fetch_callgraph(n_clicks, out_dir, jar_dir, edge_filter, jar_filter, main_identifier):
+def create_callgraph(n_clicks, out_dir, jar_dir, edge_filter, jar_filter, main_identifier, graph_state, import_dir):
     if n_clicks:
         print("Trying to fetch callgraph")
         args = Args(
@@ -102,28 +139,46 @@ def fetch_callgraph(n_clicks, out_dir, jar_dir, edge_filter, jar_filter, main_id
             graph_output_folder=Path(out_dir).resolve(),
         )
         cg = args.run_callgraph(GEN_CALLGRAPH_JAR)
-        return {
-            "nodes": cg.node_props.to_dict(orient='index'),
-            "edges": cg.edges.to_dict(orient='records')
-        }
-    return None
+        return dict(graph_state=f"Created a graph with {len(cg.node_props)} nodes and {len(cg.edges)} edges ",
+                    import_dir=out_dir)
+    return dict(graph_state=graph_state, import_dir=import_dir)
+
 
 @app.callback(
-    Output("graph_state", "children"),
-    Input("callgraph", "data")
+    dict(cytoscape=Output("cytoscape", "elements"),
+         max_nodes=Output("max_nodes", "max"),
+         max_edges=Output("max_edges", "max")
+         ),
+    Input("import", "n_clicks"),
+    State("import_dir", "value"),
+    State("max_nodes", "value"),
+    State("max_edges", "value"),
+    State("hierch", "value")
 )
-def graph_state(callgraph):
-    if not callgraph:
-        return "Not loaded yet"
-    return f'Graph with {len(callgraph["nodes"])} nodes and {len(callgraph["edges"])} edges'
+def import_graph(n_clicks, import_dir, max_nodes, max_edges, hierch):
+    if n_clicks:
+        raw_graph = RawGraph.from_folder(Path(import_dir).resolve())
+        graph = raw_graph_to_igraph(raw_graph, HierarchyType[hierch])
+        elements = igraph_to_cytoscape_fig(graph, max_nodes or 0, max_edges or 0)
+        return dict(cytoscape=elements, max_nodes=len(graph.vs), max_edges=len(graph.es))
+    return dict(cytoscape=[], max_nodes=max_nodes or DEFAULT_MAX_NODES, max_edges=max_edges or DEFAULT_MAX_EDGES)
 
 app.layout = html.Div(children=[
+    dcc.Store(id="cur_raw_graph", storage_type="memory"),
     html.H1(children='Needle in a Data Haystack: Analysis of Java call graph'),
+    graph_create,
     graph_import,
-    graph_state_div,
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
+    cyto.Cytoscape(
+        id='cytoscape',
+        layout={'name': 'cose'},
+        style={'width': '100%', 'height': '70vh'},
+        elements=[
+            {'data': {'id': 'one', 'label': 'Node 1'},
+                'position': {'x': 75, 'y': 75}},
+            {'data': {'id': 'two', 'label': 'Node 2'},
+             'position': {'x': 200, 'y': 200}},
+            {'data': {'source': 'one', 'target': 'two'}}
+        ]
     )
 ])
 
--- graph_import.py
+++ graph_import.py
@@ -1,8 +1,8 @@
 # %%
-
+from __future__ import annotations
 import subprocess
 from git_analysis import HierarchyType
-from typing import NamedTuple, List
+from typing import Any, Dict, NamedTuple, List
 from pathlib import Path
 import pandas as pd
 import sys
@@ -13,20 +13,47 @@ from dataclasses import dataclass
 
 log = logging.getLogger("graph_import")
 
-class RawCallgraph(NamedTuple):
+class RawGraph(NamedTuple):
+    """ A raw representation of a directed graph as a frame of edges ('src' and 'tgt' columns)
+        and a frame of node properties ('class', 'method', 'package' columns),
+        can be easily (de)serialized to disk.
+        
+    """
     edges: pd.DataFrame
     node_props: pd.DataFrame
 
-    def to_python_graph(self, hierch: HierarchyType) -> igraph.Graph:
-        # delete duplicate edges, we don't care if a method calls another
-        # more than once
+    @staticmethod
+    def from_folder(folder: Path) -> RawGraph:
+        edges_path = folder / "edges.json"
+        node_map_path = folder / "mapping.json"
+        edges = pd.read_json(edges_path, encoding='utf-8', orient="records")
+        node_map = pd.read_json(node_map_path, encoding='utf-8', orient='index')
+        cg = RawGraph(
+            edges=edges,
+            node_props=node_map
+        )
+        return cg
 
-        edges = self.edges[["src", "tgt"]].drop_duplicates()
+    def to_folder(self, folder: Path):
+        folder.mkdir(parents=True, exist_ok=True)
+        self.edges.to_json(folder / "edges.json", orient="records")
+        self.node_props.to_json(folder / "node_props.json", orient="index")
+
+def raw_graph_to_igraph(raw: RawGraph, hierch: HierarchyType) -> igraph.Graph:
+        edges = raw.edges[["src", "tgt"]]
         edges["weight"] = 1
-        vertices = self.node_props.reset_index()
+        vertices = raw.node_props.reset_index()
 
         graph = igraph.Graph.DataFrame(edges, directed=True,
                                        vertices=vertices)
+
+        # Note: parallel edges/self loops not possible in association rule model
+        # so this explanation is only relevant for call graph:
+        # 
+        # We consider duplicate calls from one method to another, or self loops(recursive methods)
+        # an implementation detail and not something that hints a stronger function depdenency, so
+        # we remove them.
+        graph.simplify(multiple=True, loops=True, combine_edges="first")
 
         contraction = {
             HierarchyType.method: None,
@@ -40,11 +67,64 @@ class RawCallgraph(NamedTuple):
             if contraction == "class":
                 combine_attrs["class"] = "first"
             mapping_vector, _ = pd.factorize(graph.vs[contraction], sort=False)
-            graph.contract_vertices(mapping_vector, combine_attrs )
+            graph.contract_vertices(mapping_vector, combine_attrs)
+            
+            # at this point, the contracted graph(class/package) is likely
+            # to include self-loops. These loops might be indicative of node importance,
+            # e.g, a lot of activity or some kind of god class/package (in the callgraph model),
+            # or something that changes very often(git diff model) so we may want to keep them.
+            # We do combine parallel edges though, they just clutter the graph
+            graph.simplify(multiple=True, loops=False, combine_edges="sum")
 
+        # page rank for node importance
+        graph.vs["pr"] = graph.pagerank(directed=True, weights="weight", damping=0.85,
+                                        implementation="prpack")
 
+        # TODO community detection for clustering
 
         return graph
+
+def igraph_to_cytoscape_fig(graph: igraph.Graph, max_nodes: int, max_edges: int) -> List[Dict[str, Any]]:
+    # first, pick up to 'max_nodes' with the highest importance
+    vdf = graph.get_vertex_dataframe()
+    vdf.sort_values(by="pr", ascending=False, inplace=True)
+
+    inferred_hierch = None
+    if "name" in vdf.columns:
+        inferred_hierch = HierarchyType.method
+    elif "class" in vdf.columns:
+        inferred_hierch = HierarchyType.type_def
+    else:
+        inferred_hierch = HierarchyType.package
+
+    chosen_ix = vdf[:max_nodes].index
+
+    # now, delete edges with the lowest weights so we hav 
+    # no more than 'max_edges' edges.
+    subgraph = graph.induced_subgraph(chosen_ix)
+    edf = subgraph.get_edge_dataframe()
+    edf.sort_values(by="weight", ascending=True, inplace=True)
+    n_edges_to_delete = max(len(edf) - max_edges, 0)
+    subgraph.delete_edges(edf[:n_edges_to_delete].index)
+
+    # converting to cytoscape element format
+    def vertex_name(v):
+        if inferred_hierch == HierarchyType.method:
+            return v["name"]
+        elif inferred_hierch == HierarchyType.type_def:
+            return f"{v['package']}.{v['class']}"
+        return v["package"]
+    
+    classes = inferred_hierch.name
+
+    edges = [{'data': {'source': str(edge.source), 'target': str(edge.target)}}
+             for edge in subgraph.es]
+    nodes = [{'data': {'id': str(vertex.index), 'label': vertex_name(
+        vertex), 'classes': classes}} for vertex in subgraph.vs]
+
+
+    return edges + nodes
+
 
 @dataclass
 class Args:
@@ -54,7 +134,7 @@ class Args:
     main_class_identifier: str
     graph_output_folder: Path
 
-    def run_callgraph(self, genCallgraph_jar_path: Path, force_regen: bool = False) -> RawCallgraph:
+    def run_callgraph(self, genCallgraph_jar_path: Path, force_regen: bool = False) -> RawGraph:
         args = ["java", "-jar", str(genCallgraph_jar_path)]
 
         args.extend(["-i", str(self.input_jar_folder)])
@@ -81,7 +161,7 @@ class Args:
         edges = pd.read_json(edges_path, encoding='utf-8', orient="records")
         node_map = pd.read_json(node_map_path, encoding='utf-8', orient='index')
 
-        cg = RawCallgraph(
+        cg = RawGraph(
             edges = edges,
             node_props= node_map
         )
@@ -105,6 +185,10 @@ def run(refresh=False):
     cg = args.run_callgraph(GEN_CALLGRAPH_JAR, force_regen=refresh)
     return cg
 
+
+# %%
+cg = run()
+g = raw_graph_to_igraph(cg, HierarchyType.package)
 # %%
 
 if __name__ == "__main__":
