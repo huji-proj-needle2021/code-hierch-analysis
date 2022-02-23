1e604f7e05f09d8cf3de1992b7eb8d9d0f93ad01
Daniel Kerbel <nmdanny@gmail.com>
2022-02-23 17:38:28 +0200

    refactor graph import, integrate with assoc mining

--- git_analysis/assoc_mining.py
+++ git_analysis/assoc_mining.py
@@ -1,4 +1,6 @@
-from typing import Any, Mapping, Optional, Iterable, List
+from typing import Any, Dict, Mapping, NamedTuple, Optional, Iterable, List, Set, Tuple
+
+from .git_processor import GitProcessor
 from .java_type import JavaIdentifier
 from .java_change import HierarchyType, JavaChange
 from mlxtend.frequent_patterns import apriori, fpgrowth, fpmax, association_rules
@@ -6,17 +8,18 @@ import logging
 from tqdm import tqdm
 import pandas as pd
 import numpy as np
-import networkx as nx
+from graph_import import GraphData
+import json
 
 log = logging.getLogger("assoc_mining")
 
 def convert_to_item_basket_onehot(commits_or_prs: Iterable[Mapping[str, Any]],
                                   hierarchy_type: HierarchyType,
-                                  commit_or_pr: str) -> pd.DataFrame:
+                                  commit_or_pr: str) -> Tuple[pd.DataFrame, Mapping[str, JavaIdentifier]]:
     """ Given an iterable of commits(for each commit-parent pair) or pull requests
         that were processed by GitProcessor, converts them to a Pandas dataframe
         with a one-hot encoding of the baskets, suitable for use by frequent itemsets
-        algorithms.
+        algorithms. Also returns a mapping between columns(items) to their full JavaIdentifier objects
 
         Each row represents a basket - commit or PR
         Each column represents a possible item, there's one row for each Java Identifier in the program.
@@ -48,6 +51,12 @@ def convert_to_item_basket_onehot(commits_or_prs: Iterable[Mapping[str, Any]],
             "basket_id": obj[index_col],
             "item": change
         } for change in changes))
+
+    str_to_iden = {
+        str(rec["item"]): rec["item"] 
+        for rec in records
+    }
+
     df = pd.DataFrame.from_records(records)
     df.set_index("basket_id", inplace=True)
     sparse_type = pd.SparseDtype(np.bool8, False)
@@ -56,4 +65,141 @@ def convert_to_item_basket_onehot(commits_or_prs: Iterable[Mapping[str, Any]],
     
     log.info(f"item type: {hierarchy_type.name}, basket type: {commit_or_pr}, #baskets: {len(onehot)}, #items: {len(onehot.columns)}")
 
-    return onehot
+    return onehot, str_to_iden
+
+
+class ModelParams(NamedTuple):
+    commit_or_pr: str
+    hierarchy_type: HierarchyType
+    min_support: float
+    min_confidence: float
+
+class Model(NamedTuple):
+    """ An association rules model, along with all data used to
+        create it.
+    """
+    params: ModelParams
+    one_hot: pd.DataFrame
+    col_mapping: Mapping[str, JavaIdentifier]
+    freq_itemsets: pd.DataFrame
+    assoc_rules: pd.DataFrame
+
+    def describe(self) -> str:
+        return (f"#baskets: {len(self.one_hot)} #items: {len(self.one_hot.columns)} "
+                f"#freq itemsets: {len(self.freq_itemsets)} #rules: {len(self.assoc_rules)}")
+
+def model_to_graph_data(model: Model) -> GraphData:
+    pass
+
+class AssocAnalyzer:
+    """ Helper class for analyzing git changes via association rules.
+        Mostly used to cache computations, 
+    """
+
+    def __init__(self, git_url: str):
+        self._git_url = git_url
+        with GitProcessor(git_url) as git:
+            self._commits, _commits_by_id = git.get_processed_commits()
+            self._prs = git.get_processed_prs()
+        
+
+        # caches to avoid re-calculating stuff
+        self._onehots: Dict[Tuple[str, HierarchyType], Tuple[pd.DataFrame, Mapping[str, JavaIdentifier]]] = {}
+        self._freq_itemsets: Dict[Tuple[str, HierarchyType, float], pd.DataFrame] = {}
+        self._models: Dict[ModelParams, Model] = {}
+
+    def _get_onehot(self, commit_or_pr: str, hierch_type: HierarchyType) -> Tuple[pd.DataFrame, Mapping[str, JavaIdentifier]]:
+        """ Cached method for getting/creating an item-basket frame """
+        assert commit_or_pr in ("commit", "pr")
+        res = self._onehots.get((commit_or_pr, hierch_type), None)
+        if res is None:
+            collec = self._prs if commit_or_pr == "pr" else self._commits
+            res = convert_to_item_basket_onehot(collec, hierch_type, commit_or_pr)
+            self._onehots[(commit_or_pr, hierch_type)] = res
+        return res
+    
+    def _get_freq_itemsets(self, commit_or_pr: str, hierch_type: HierarchyType, min_support: float) -> pd.DataFrame:
+        """ Cached method for getting/creating  frequent itemsets frame """
+        df = self._freq_itemsets.get((commit_or_pr, hierch_type, min_support), None)
+        if df is None:
+            one_hot, _ = self._get_onehot(commit_or_pr, hierch_type)
+            df = apriori(one_hot, min_support=min_support,
+                                    max_len=2, use_colnames=True, verbose=1)
+            self._freq_itemsets[(commit_or_pr, hierch_type, min_support)] = df
+        return df
+
+
+    def analyze_model(self, model_params: Optional[ModelParams] = None, **kwargs) -> Model:
+        """ Cached method for getting/creating association rules using the given model
+            parameters
+        """
+        if model_params is None:
+            model_params = ModelParams(**kwargs)
+        
+        model = self._models.get(model_params, None)
+        if model:
+            return model
+
+        one_hot, mapping = self._get_onehot(model_params.commit_or_pr, model_params.hierarchy_type)
+        freq_itemsets = self._get_freq_itemsets(model_params.commit_or_pr, model_params.hierarchy_type, 
+                                                model_params.min_support)
+        rules = association_rules(freq_itemsets, metric='confidence', min_threshold=model_params.min_confidence)
+        rules["antecedents"] = rules["antecedents"].apply(lambda s: next(iter(s)))
+        rules["consequents"] = rules["consequents"].apply(lambda s: next(iter(s)))
+
+        model = Model(
+            params=model_params,
+            one_hot=one_hot,
+            col_mapping=mapping,
+            freq_itemsets=freq_itemsets,
+            assoc_rules=rules
+        )
+        self._models[model_params] = model
+        return model
+
+    def create_graph(self, model: Model) -> GraphData:
+        """ Creates a graph using associatino rules model,
+            the graph might be persisted to disk or further analyzed.
+        """
+        model.assoc_rules["weight"] = model.assoc_rules["confidence"]
+        edges = model.assoc_rules
+
+
+        seenVertices: Set[str] = set()
+        vertex_records = []
+        for row in model.assoc_rules[["antecedents", "consequents"]].itertuples():
+            nodes = set((row.consequents, row.antecedents)) - seenVertices
+            seenVertices.update(nodes)
+            for node in nodes:
+                iden = model.col_mapping[node]
+                record = {
+                    "identifier": node
+                }
+                if model.params.hierarchy_type == HierarchyType.method:
+                    record["method"] = iden.hierarchies[0][1]
+                    record["class"] = ".".join(h[1] for h in iden.hierarchies[1:-1])
+                    record["package"] = iden.hierarchies[-1][1]
+                elif model.params.hierarchy_type == HierarchyType.type_def:
+                    record["class"] = ".".join(h[1] for h in iden.hierarchies[:-1])
+                    record["package"] = iden.hierarchies[-1][1]
+                else:
+                    record["package"] = iden.hierarchies[0][1]
+                vertex_records.append(record)
+            
+        vertices = pd.DataFrame.from_records(vertex_records, index="identifier")
+
+        
+
+        attrs = {
+            "git_url": self._git_url,
+            **model.params._asdict()
+        } 
+        hash_val = hash(json.dumps(attrs))
+        return GraphData(
+            vertices=vertices,
+            edges=edges,
+            hierch=model.params.hierarchy_type,
+            type="git_changes",
+            attrs=attrs,
+            hash=hash_val
+        )
--- graph_import.py
+++ graph_import.py
@@ -1,131 +1,82 @@
 # %%
 from __future__ import annotations
 import subprocess
-from git_analysis import HierarchyType
-from typing import Any, Dict, NamedTuple, List
+from git_analysis.java_type import HierarchyType
+from typing import Any, Mapping, NamedTuple, List, Optional
 from pathlib import Path
 import pandas as pd
 import sys
 import logging
-import igraph
 from dataclasses import dataclass
+import json
 
 
 log = logging.getLogger("graph_import")
 
-class RawGraph(NamedTuple):
-    """ A raw representation of a directed graph as a frame of edges ('src' and 'tgt' columns)
-        and a frame of node properties ('class', 'method', 'package' columns),
-        can be easily (de)serialized to disk.
-        
+GRAPH_DIR = Path(__file__).parent / "GRAPHS"
+
+class GraphData(NamedTuple):
+    """ An unprocessed graph of Java hierarchies, whether made from git analysis or
+        a callgraph.
     """
     edges: pd.DataFrame
-    node_props: pd.DataFrame
+    vertices: pd.DataFrame
+
+    hierch: HierarchyType
+
+    # callgraph/git changes
+    type: str
+
+    # hash of the parameters, for caching
+    hash: int
+
+    # parameters used in generating the graph
+    attrs: Mapping[str, Any]
 
     @staticmethod
-    def from_folder(folder: Path) -> RawGraph:
+    def from_folder(folder: Path) -> GraphData:
         edges_path = folder / "edges.json"
         node_map_path = folder / "mapping.json"
+        meta_path = folder / "meta.json"
+
         edges = pd.read_json(edges_path, encoding='utf-8', orient="records")
         node_map = pd.read_json(node_map_path, encoding='utf-8', orient='index')
-        cg = RawGraph(
+        with open(meta_path, 'r', encoding='utf-8') as meta_file:
+            meta_obj = json.load(meta_file)
+        cg = GraphData(
             edges=edges,
-            node_props=node_map
+            vertices=node_map,
+            hierch=HierarchyType[meta_obj["hierch"]],
+            type=meta_obj["type"],
+            hash=meta_obj["hash"],
+            attrs=meta_obj["attrs"]
         )
         return cg
 
     def to_folder(self, folder: Path):
         folder.mkdir(parents=True, exist_ok=True)
-        self.edges.to_json(folder / "edges.json", orient="records")
-        self.node_props.to_json(folder / "node_props.json", orient="index")
+        edges_path = folder / "edges.json"
+        node_map_path = folder / "mapping.json"
+        meta_path = folder / "meta.json"
 
-def raw_graph_to_igraph(raw: RawGraph, hierch: HierarchyType) -> igraph.Graph:
-        edges = raw.edges[["src", "tgt"]]
-        edges["weight"] = 1
-        vertices = raw.node_props.reset_index()
+        self.edges.to_json(edges_path, orient="records")
+        self.vertices.to_json(node_map_path, orient="index")
 
-        graph = igraph.Graph.DataFrame(edges, directed=True,
-                                       vertices=vertices)
-
-        # Note: parallel edges/self loops not possible in association rule model
-        # so this explanation is only relevant for call graph:
-        # 
-        # We consider duplicate calls from one method to another, or self loops(recursive methods)
-        # an implementation detail and not something that hints a stronger function depdenency, so
-        # we remove them.
-        graph.simplify(multiple=True, loops=True, combine_edges="first")
-
-        contraction = {
-            HierarchyType.method: None,
-            HierarchyType.type_def: "class",
-            HierarchyType.package: "package",
-        }[hierch]
-        if contraction:
-            combine_attrs = {
-                "package": "first"
-            }
-            if contraction == "class":
-                combine_attrs["class"] = "first"
-            mapping_vector, _ = pd.factorize(graph.vs[contraction], sort=False)
-            graph.contract_vertices(mapping_vector, combine_attrs)
-            
-            # at this point, the contracted graph(class/package) is likely
-            # to include self-loops. These loops might be indicative of node importance,
-            # e.g, a lot of activity or some kind of god class/package (in the callgraph model),
-            # or something that changes very often(git diff model) so we may want to keep them.
-            # We do combine parallel edges though, they just clutter the graph
-            graph.simplify(multiple=True, loops=False, combine_edges="sum")
-
-        # page rank for node importance
-        graph.vs["pr"] = graph.pagerank(directed=True, weights="weight", damping=0.85,
-                                        implementation="prpack")
-
-        # TODO community detection for clustering
-
-        return graph
-
-def igraph_to_cytoscape_fig(graph: igraph.Graph, max_nodes: int, max_edges: int) -> List[Dict[str, Any]]:
-    # first, pick up to 'max_nodes' with the highest importance
-    vdf = graph.get_vertex_dataframe()
-    vdf.sort_values(by="pr", ascending=False, inplace=True)
+        with open(meta_path, 'w', encoding='utf-8') as meta_file:
+            json.dump({
+                "type": self.type,
+                "hierch": self.hierch.name,
+                "attrs": self.attrs,
+                "hash": self.hash
+            }, meta_file)
 
 
-    chosen_ix = vdf[:max_nodes].index
-
-    # now, pick up to 'max_edges' edges with highest weights.
-    # nodes with no edges will be removed
-    subgraph = graph.induced_subgraph(chosen_ix)
-    edf = subgraph.get_edge_dataframe()
-    edf.sort_values(by="weight", ascending=False, inplace=True)
-
-    subgraph = subgraph.subgraph_edges(edf[:max_edges].index, delete_vertices=True)
 
 
-    inferred_hierch = None
-    if "name" in vdf.columns:
-        inferred_hierch = HierarchyType.method
-    elif "class" in vdf.columns:
-        inferred_hierch = HierarchyType.type_def
-    else:
-        inferred_hierch = HierarchyType.package
-    # converting to cytoscape element format
-    def vertex_name(v):
-        if inferred_hierch == HierarchyType.method:
-            return v["class"] + "." + v["method"]
-        elif inferred_hierch == HierarchyType.type_def:
-            return v["class"]
-        return v["package"]
-    
-    classes = inferred_hierch.name
-
-    edges = [{'data': {'source': str(edge.source), 'target': str(edge.target)}}
-             for edge in subgraph.es]
-    nodes = [{'data': {'id': str(vertex.index), 'label': vertex_name(
-        vertex), 'classes': classes}} for vertex in subgraph.vs]
-
-
-    return edges + nodes
-
+def path_contains_graph(path: Path) -> bool:
+    return ((path / "edges.json").exists() and
+            (path / "mapping.json").exists() and
+            (path / "meta.json").exists())
 
 @dataclass
 class Args:
@@ -135,7 +86,7 @@ class Args:
     main_class_identifier: str
     graph_output_folder: Path
 
-    def run_callgraph(self, genCallgraph_jar_path: Path, force_regen: bool = False) -> RawGraph:
+    def run_callgraph(self, genCallgraph_jar_path: Path, force_regen: bool = False) -> GraphData:
         args = ["java", "-jar", str(genCallgraph_jar_path)]
 
         args.extend(["-i", str(self.input_jar_folder)])
@@ -147,31 +98,36 @@ class Args:
         if len(self.jar_filter) > 0:
             args.extend(["--jar-filter"] + self.jar_filter)
         
-        edges_path = self.graph_output_folder / "edges.json"
-        node_map_path = self.graph_output_folder / "mapping.json"
-
-        if not edges_path.exists() or not node_map_path.exists() or force_regen:
+        if not path_contains_graph(self.graph_output_folder) or force_regen:
             log.info("Generating callgraphs")
             ret = subprocess.call(args)
             if ret != 0:
                 log.error(f"Generating callgraphs failed")
                 sys.exit(ret)
+            
+            # on success, generate metadata for caching purposes
+            with open(self.graph_output_folder / "meta.json", 'w', encoding='utf-8') as meta:
+                attrs = {**self.__dict__ }
+                for k, v in attrs.items():
+                    if isinstance(v, Path):
+                        attrs[k] = str(v)
+                hash_val = hash(json.dumps(attrs))
+                json.dump({
+                    "type": "callgraph",
+                    "hierch": HierarchyType.method.name,
+                    "attrs": attrs,
+                    "hash": hash_val
+                }, meta)
         else:
             log.info(f"Using pre-existing graph files at {self.graph_output_folder}")
         
-        edges = pd.read_json(edges_path, encoding='utf-8', orient="records")
-        node_map = pd.read_json(node_map_path, encoding='utf-8', orient='index')
-
-        cg = RawGraph(
-            edges = edges,
-            node_props= node_map
-        )
-        log.info(f"Call graph has {len(cg.node_props)} nodes and {len(cg.edges)} edges")
+        cg = GraphData.from_folder(self.graph_output_folder)
+        log.info(f"{cg.type} has {len(cg.vertices)} nodes and {len(cg.edges)} edges")
         return cg
 
 GEN_CALLGRAPH_JAR = Path(__file__).resolve().parent / "genCallgraph.jar"
 JAR_FOLDER = Path(__file__).resolve().parent.parent / "genCallgraph" / "toAnalyze"
-OUTPUT_FOLDER = Path(__file__).resolve().parent / "graphImport"
+OUTPUT_FOLDER = GRAPH_DIR / "jadx"
 
 def run(refresh=False):
     logging.basicConfig()
@@ -187,11 +143,6 @@ def run(refresh=False):
     return cg
 
 
-# %%
-cg = run()
-g = raw_graph_to_igraph(cg, HierarchyType.package)
-# %%
-
 if __name__ == "__main__":
     cg = run()
-    print(cg.node_props)
+    print(cg.vertices)
